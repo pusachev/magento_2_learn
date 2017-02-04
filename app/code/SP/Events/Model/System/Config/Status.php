cc20a0d17f3fcf4749b066a581053b08061ff3f2
Pavel Usachev <pusachev@oroinc.com>
2017-02-04 09:42:21 +0200

    Added backend part

--- app/code/SP/Events/Model/System/Config/Status.php
+++ app/code/SP/Events/Model/System/Config/Status.php
@@ -6,8 +6,23 @@
 
 namespace SP\Events\Model\System\Config;
 
+use Magento\Framework\Option\ArrayInterface;
 
-class Status
+class Status implements ArrayInterface
 {
+    const ENABLED  = 1;
+    const DISABLED = 0;
 
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options = [
+            self::ENABLED => __('Enabled'),
+            self::DISABLED => __('Disabled')
+        ];
+
+        return $options;
+    }
 }
